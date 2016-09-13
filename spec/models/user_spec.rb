1cd658ef71bcbbe19970f7336b347601e5360e40
alexavlonitis <avlonitis@msn.com>
2016-09-13 16:40:23 +0100

    added user first specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:user) { described_class.new }
+
+  let(:user) { FactoryGirl.create(:user) }
+
+  it { should respond_to(:email) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should be_valid }
 end
